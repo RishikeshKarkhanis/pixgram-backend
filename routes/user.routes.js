ad901c72030c083ad3c79e9863e516297a2ff9d2
Rishikesh Karkhanis <rishikeshkarkhanis0101@gmail.com>
2025-08-23 00:21:30 +0530

    Made Logout System

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const { createUser, getUsers, deleteUser, updateUser, loginUser } = require('../controllers/user.controller.js');
-const {getUser} = require('../services/auth.js')
+const { getUser } = require('../services/auth.js')
 
 const router = express.Router();
 
 router.get('/currentUser', (req, res) => {
     const token = req.cookies.uid;
-    
+
     const user = getUser(token);
     if (!user) {
-        return res.status(401).send({"error": "Unauthorized"});
+        return res.status(401).send({ "error": "Unauthorized" });
     }
     res.send(user);
 });
@@ -17,24 +17,24 @@ router.get('/currentUser', (req, res) => {
 router.post('/register', async (req, res) => {
     const userData = req.body;
     const user = await createUser(userData);
-    if(user === null) {
-        return res.status(400).send({"error": "User creation failed"});
-        
+    if (user === null) {
+        return res.status(400).send({ "error": "User creation failed" });
+
     } else {
         res.status(201).send(user);
     }
 });
 
 router.post('/login', async (req, res) => {
-    
+
     const user = await loginUser(req, res);
 
     if (user === null) {
         return res.status(401).send("Login failed: Invalid credentials");
     }
-    
+
     res.send(`Login successful for user: ${user}`);
-    
+
 });
 
 router.delete('/delete/:id', async (req, res) => {
@@ -48,6 +48,13 @@ router.put('/update/:id', async (req, res) => {
     const userData = req.body;
     updateUser(userId, userData);
     res.send("User updated successfully");
+});
+
+router.post('/logout', async (req, res) => {
+    res.clearCookie("uid");
+    res.status(200).json({ message: "Logged out successfully" });
+    console.log("user logged out!");
+    
 });
 
 router.get('/', (req, res) => {
